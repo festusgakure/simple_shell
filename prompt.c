81ebca32248694e2c89fae6d0609465c6a7ec1e5
iddokip <kiptarusiddo@gmail.com>
2023-05-24 13:03:19 -0700

    ..

--- prompt.c
+++ prompt.c
@@ -35,7 +35,8 @@ void prompt(char **av, char **env)
 			free(string);
 			exit(EXIT_SUCCESS);
 		}
-		fpid = fork();
+		if (Handle_Path(args) == 0)
+			fpid = fork();
 		if (fpid == -1)
 		{
 			free(string);
